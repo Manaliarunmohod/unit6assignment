4a90a2acc281c5315170db01a240dc21a694af74
Manali mohod <masaimanali@gmail.com>
2022-05-17 14:56:01 +0530

    change

---
+++ contact-application/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Contactlist } from './Components/Contactlist';
+
+function App() {
+  return (
+    <div className="App">
+     <Contactlist/>
+    </div>
+  );
+}
+
+export default App;
