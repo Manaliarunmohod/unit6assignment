97798cdec58a54a11b53429a9b7244a3d9ac16e0
Manali mohod <masaimanali@gmail.com>
2022-05-20 14:20:46 +0530

    changes

---
+++ restaurent/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+ import  {Food}  from './Component/Food'
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Food/>
+      
+    </div>
+  )
+}
+
+export default App
